96dbf8fc3be2ffd3722ffaaee47ee7d37c8ee122
alberdinamariya <mariyaalberdina1980@gmail.com>
2022-04-07 16:57:20 +0300

    7 april

--- sqrtfirst/squrteq/serializers.py
+++ sqrtfirst/squrteq/serializers.py
@@ -7,3 +7,6 @@ class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Forsqrt
         fields = ('first_number', 'second_number', 'third_number', 'res_1', 'res_2', 'unsolvable')
+
+    def create(self, validated_data):
+        return Forsqrt.objects.create(**validated_data)
